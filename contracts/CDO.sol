36569a80738dff922432242668bed817b6c0fb3f
Noah Passalacqua <noah.passa@gmail.com>
2017-10-12 14:49:26 -0400

    Payout tranches. Senior in full then mezzanine.

--- contracts/CDO.sol
+++ contracts/CDO.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.4.8;
 
 import "./CDOLib.sol";
 import "./RedeemableTokenLib.sol";
-import "./LoanRegistry.sol";
 
 contract CDO {
 
   using SafeMath for uint;
+  using RedeemableTokenLib for RedeemableTokenLib.Accounting;
   using CDOLib for CDOLib.CDO;
 
   event CDOCreated(
@@ -20,12 +20,44 @@ contract CDO {
   }
 
   function create(bytes32 uuid, bytes32[] loan_ids) {
-    CDOLib.CDO cdo = cdos[uuid];
-
-    cdo.initialize();
-    cdo.loan_ids = loan_ids;
+    cdos[uuid].initialize(loan_ids);
 
     CDOCreated(uuid, block.number);
+  }
+
+  function repayment(bytes32 uuid) payable {
+    CDOLib.CDO cdo = cdos[uuid];
+
+    uint amountLeft = cdo.seniorTranche.totalWorth - cdo.seniorTranche.token.totalValueAccrued;
+
+    // Payout the senior tranche in full first.
+    if(amountLeft > 0) {
+      // If sent value goes over senior total worth, pay rest to mezzanine.
+      // Otherwise, pay full amount to senior.
+      if(msg.value > amountLeft) {
+        paySeniorTranche(cdo, amountLeft);
+        payMezzanineTranche(cdo, msg.value - amountLeft);
+      } else {
+        paySeniorTranche(cdo, msg.value);
+      }
+    } else {
+      payMezzanineTranche(cdo, msg.value);
+    }
+  }
+
+  function paySeniorTranche(CDOLib.CDO cdo, uint amount) internal {
+    cdo.seniorTranche.token.totalValueAccrued =
+      cdo.seniorTranche.token.totalValueAccrued.add(amount);
+  }
+
+  function payMezzanineTranche(CDOLib.CDO cdo, uint amount) internal {
+    cdo.seniorTranche.token.totalValueAccrued =
+      cdo.seniorTranche.token.totalValueAccrued.add(amount);
+  }
+
+  function redeemInvestment(bytes32 uuid) {
+    cdos[uuid].seniorTranche.token.redeemValue(uuid, msg.sender);
+    cdos[uuid].mezzanineTranche.token.redeemValue(uuid, msg.sender);
   }
 
 }
--- contracts/CDOLib.sol
+++ contracts/CDOLib.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.8;
 
+import "./SafeMath.sol";
 import "./RedeemableTokenLib.sol";
 
 library CDOLib {
 
+  using SafeMath for uint;
   using RedeemableTokenLib for RedeemableTokenLib.Accounting;
 
   uint constant seniorSupply = 600000;
@@ -11,6 +13,7 @@ library CDOLib {
 
   struct Tranche {
     RedeemableTokenLib.Accounting token;
+    uint totalWorth;
   }
 
   struct CDO {
@@ -20,8 +23,15 @@ library CDOLib {
     Tranche mezzanineTranche;
   }
 
-  function initialize(CDO storage self) {
+  function initialize(CDO storage self, bytes32[] loan_ids) {
     self.owner = msg.sender;
+    self.loan_ids = loan_ids;
+
+    // Determine tranches total worth
+    uint totalWorth = 6 ether;
+    totalWorth = totalWorth.add(totalWorth.mul(4).div(100)); // 4% interest
+    self.seniorTranche.totalWorth = totalWorth.mul(6).div(10); // 60%
+    self.mezzanineTranche.totalWorth = totalWorth.mul(4).div(10); // 40%
 
     self.seniorTranche.token.totalSupply = seniorSupply;
     self.seniorTranche.token.balances[msg.sender] = seniorSupply;
